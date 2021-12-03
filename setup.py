334230a2171e728927146a3e5e56ccfb9b0d96bf
haggishunk <travis@mattera.io>
2021-12-02 22:04:59 -0800

    Updated requirements

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     author_email="travis@mattera.io",
     packages=find_packages(include=["directo", "directo.*"]),
     install_requires=[
-        "jinja2",
-        "google-auth-oauthlib",
         "google-api-python-client",
+        "google-auth-httplib2",
         "google-auth-oauthlib",
+        "jinja2",
     ],
     entry_points={"console_scripts": ["directo=directo.main:main"]},
 )
